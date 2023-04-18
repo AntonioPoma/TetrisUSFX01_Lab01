d7a7dd75c45d18e3a19ab9b279ec20ff5beebfdc
AntonioPoma <127070232+AntonioPoma@users.noreply.github.com>
2023-04-18 09:08:22 -0400

    Agregar archivos de proyecto.

---
+++ Intermediate/Build/Win64/TetrisUSFX01Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.35.32215
+#include "C:/Users/anton/Downloads/TetrisUSFX_para_el_examen-master/TetrisUSFX01-master/Intermediate/Build/Win64/TetrisUSFX01Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
